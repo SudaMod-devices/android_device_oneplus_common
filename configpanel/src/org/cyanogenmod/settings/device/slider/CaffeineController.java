fff381c86fc82bec6f7b79e57bc24cc0eb3077b7
wuyi <wuyi@ligux.com>
2017-09-11 20:03:16 +0800

    change to cyanogenmod
    Change-Id: I81ea260623831bafb5b4f3d53a0ac923467ab7fa

--- configpanel/src/org/cyanogenmod/settings/device/slider/CaffeineController.java
+++ configpanel/src/org/cyanogenmod/settings/device/slider/CaffeineController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.mokee.settings.device.slider;
+package org.cyanogenmod.settings.device.slider;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -25,7 +25,7 @@ import android.util.Log;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 
-import org.mokee.settings.device.SliderControllerBase;
+import org.cyanogenmod.settings.device.SliderControllerBase;
 
 public final class CaffeineController extends SliderControllerBase {
 
